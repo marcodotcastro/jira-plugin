722cb51d4c701a166c4746ebd484022da241db1a
Marco Castro <marcodotcastro@gmail.com>
2018-09-28 16:46:52 -0300

    adicionado keyshort

--- src/main/java/br/com/jira/hello/servlet/Hello.java
+++ src/main/java/br/com/jira/hello/servlet/Hello.java
@@ -8,7 +8,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.inject.Inject;
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.user.ApplicationUser;
+import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.atlassian.templaterenderer.TemplateRenderer;
 import java.io.IOException;
+import java.util.*;
 
 @Scanned
 public class Hello extends HttpServlet{
